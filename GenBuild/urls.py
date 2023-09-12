a102020347c5420d15b9ac5a11ef6c57772d78c1
SapeNeCo <makarpletnev12345@yandex.ru>
2023-09-12 20:32:58 +0400

    added contacts

--- GenBuild/urls.py
+++ GenBuild/urls.py
@@ -4,6 +4,7 @@ from main import views
 urlpatterns = [
     path('', views.home),
     path('about/', views.about),
+    path('contacts/', views.contacts),
     path('reviews/', views.reviews),
     path('news/', views.news)
 ]
