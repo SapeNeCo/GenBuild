2c2c5758b964f983005503c7ba3a911b9be7ac80
SapeNeCo <makarpletnev12345@yandex.ru>
2023-09-13 21:45:39 +0400

    the victory will be ours

--- GenBuild/urls.py
+++ GenBuild/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from main import views
 
 urlpatterns = [
-    path('', views.home),
-    path('about/', views.about),
-    path('contacts/', views.contacts),
-    path('reviews/', views.reviews),
-    path('news/', views.news)
+    path('', views.home, name="home"),
+    path('about/', views.about, name="about"),
+    path('contacts/', views.contacts, name="contacts"),
+    path('reviews/', views.reviews, name="reviews"),
+    path('news/', views.news, name="news")
 ]
